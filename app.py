b7eb30bc8d7f8bb477d3d74092a504607eb41d19
Jonathan Chan <jonathan.chan41@gmail.com>
2023-08-16 15:38:00 -0600

    add both deps - save button fix

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.graph_objects as go
+import kaleido
 import plotly.express as px #if issue with plotly Figure().write_image() function: pip install -U kaleido
 from io import BytesIO
 
